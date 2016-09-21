4f45e70680b40adb6893ded4b1883e079bda0759
nice295 <nice295@gmail.com>
2016-09-21 22:01:26 +0900

    add

--- Android/app/src/main/java/com/example/leedayeon/listdetail/DetailActivity.java
+++ Android/app/src/main/java/com/example/leedayeon/listdetail/DetailActivity.java
@@ -1,42 +1,29 @@
 package com.example.leedayeon.listdetail;
 
-import android.app.ActionBar;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-/**
- * Created by LeeDaYeon on 2016-07-20.
- */
-public class DetailActivity extends AppCompatActivity {
-    Intent intent;
-    TextView tvTitle;
-    TextView tvDesc;
+import com.example.leedayeon.listdetail.R;
+
+public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button mBtJoin;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        intent = getIntent();
-        Post myPost = (Post)intent.getSerializableExtra("detail");
-
-        tvTitle = (TextView)findViewById(R.id.textView);
-        tvDesc = (TextView)findViewById(R.id.textView2);
-
-        tvTitle.setText(myPost.getTitle());
-        tvDesc.setText(myPost.getDescription());
-
-
-        ActionBar actionBar = getActionBar();
-        getSupportActionBar().setTitle(myPost.getTitle());
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mBtJoin = (Button) findViewById(R.id.btJoin);
+        mBtJoin.setOnClickListener(this);
     }
-    public boolean onOptionsItemSelected(MenuItem item) {
-        finish();
-        return  true;
+
+    @Override
+    public void onClick(View view) {
+        if (view == mBtJoin) {
+            mBtJoin.setText(getString(R.string.joining));
+        }
     }
 }
+
