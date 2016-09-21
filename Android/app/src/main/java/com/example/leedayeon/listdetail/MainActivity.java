b3e5e181b0159c215239974d559136b7c2f5f772
DianaLeee <yeony_lee@idayeon-ui-MacBook-Pro.local>
2016-09-21 21:20:47 +0900

    Connect fab
    MainActivity와 newQuizActivity 연결

--- Android/app/src/main/java/com/example/leedayeon/listdetail/MainActivity.java
+++ Android/app/src/main/java/com/example/leedayeon/listdetail/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.leedayeon.listdetail;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -57,65 +58,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(MainActivity.this);
-                LayoutInflater inflater1 = MainActivity.this.getLayoutInflater();
-                final View dialogView = inflater1.inflate(R.layout.custom_dialog, null);
-                dialogBuilder.setView(dialogView);
-
-
-                final EditText edt = (EditText) dialogView.findViewById(R.id.editTitle);
-                final EditText edt2 = (EditText) dialogView.findViewById(R.id.editDesc);
-                final EditText edt3 =(EditText)dialogView.findViewById(R.id.editDate);
-
-                RadioGroup radioGroup =(RadioGroup)dialogView.findViewById(R.id.radioGroup);
-                radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(RadioGroup group, int checkedId) {
-                        int i = -1;
-                        if (group.getId() == R.id.radioGroup) {
-                            switch (checkedId) {
-                                case R.id.radioButton:
-                                    i = 0;
-                                    break;
-                                case R.id.radioButton2:
-                                    i = 1;
-                                    break;
-                            }
-                        }
-                        id = i;
-                    }
-                });
-                dialogBuilder.setTitle("ADD");
-                dialogBuilder.setPositiveButton("ADD", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        if(edt.getText() != null) {
-
-                            if(id==0)
-                                recycler_item = new Recycler_item(R.mipmap.ic_launcher,edt.getText().toString(),edt2.getText().toString(),edt3.getText().toString(),R.mipmap.sign);
-                            else
-                                recycler_item = new Recycler_item(R.mipmap.crown,edt.getText().toString(),edt2.getText().toString(),edt3.getText().toString(),R.mipmap.sign);
-                            edt.clearFocus();
-                            edt.setText("");
-                            edt2.clearFocus();
-                            edt2.setText("");
-
-
-                        }
-                        items.add(recycler_item);
-                    }
-                });
-
-                dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        //pass
-                    }
-                });
-
-                AlertDialog b = dialogBuilder.create();
-                b.show();
-                b.getButton(b.BUTTON_NEGATIVE).setTextColor(Color.parseColor("#FFF44336"));
-                b.getButton(b.BUTTON_POSITIVE).setTextColor(Color.parseColor("#FFF44336"));
+                Intent intent = new Intent(MainActivity.this, NewQuizActivity.class);
+                startActivity(intent);
             }
         });
         Adapter = new RecyclerAdapter(getApplicationContext(),items,R.layout.item_message);
