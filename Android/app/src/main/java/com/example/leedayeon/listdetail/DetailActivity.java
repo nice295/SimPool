69b5eecf5367fb46cafc61ed5fbca2aa367748ea
jiyoung111 <jiyoung928@gmail.com>
2016-11-01 00:21:26 +0900

    detail database update

--- Android/app/src/main/java/com/example/leedayeon/listdetail/DetailActivity.java
+++ Android/app/src/main/java/com/example/leedayeon/listdetail/DetailActivity.java
@@ -10,14 +10,20 @@ import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.leedayeon.listdetail.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DetailActivity extends AppCompatActivity implements View.OnClickListener {
@@ -36,8 +42,10 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
     private String obj_2;
 
     private DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
-
+    private FirebaseAuth mAuth;
+    FirebaseUser user;
     private String games_id;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +59,10 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
 
         tvTitle = (TextView)findViewById(R.id.tvTitle);
         tvDescription = (TextView)findViewById(R.id.tvDescription);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
 
         Intent intent2 = getIntent();
         games_id = intent2.getStringExtra("games_id");
@@ -71,8 +83,35 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
                 obj_1 = map.get("obj_1");
                 Item1.setText(obj_1);
 
+
                 obj_2 = map.get("obj_2");
                 Item2.setText(obj_2);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef.child("games").child(games_id).child("participant").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Log.e("Count " ,""+dataSnapshot);
+//                Map<String, String> map = (Map)dataSnapshot.getValue();
+
+                for (DataSnapshot friendSnapshot: dataSnapshot.getChildren()) {
+                    Log.e("uid :", title +"" + friendSnapshot.getKey());
+                    if(user.getUid().equals(friendSnapshot.getKey())){
+                        mBtJoin.setText(getString(R.string.joining));
+                        mBtJoin.setEnabled(false);
+                        Item1.setEnabled(false);
+                        Item2.setEnabled(false);
+                    }
+
+                }
+
 
             }
 
@@ -89,11 +128,26 @@ public class DetailActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onClick(View view) {
         if (view == mBtJoin) {
+
+
+            int id = radioButtons.getCheckedRadioButtonId();
+            RadioButton rd = (RadioButton)findViewById(id);
+            myRef.child("games").child(games_id).child("participant").child(user.getUid()).child("answer").setValue(rd.getText().toString());
+
             mBtJoin.setText(getString(R.string.joining));
             mBtJoin.setEnabled(false);
             Item1.setEnabled(false);
             Item2.setEnabled(false);
             //Item3.setEnabled(false);
+
+//            Map<String, Object> childUpdates = new HashMap<>();
+//            childUpdates.put("/games/" + games_id, result);
+//
+//            myRef.updateChildren(childUpdates);
+//            myRef.child("games").child(games_id).child("owner").setValue("dongduk");
+
+
+
         }
     }
 }
