0fdd2906413bb7fa465f1f21cc3068fb885e48c9
Rhea10 <rea9510@gmail.com>
2016-11-02 01:09:46 +0900

    주관식일 떄 화면. DetailSubjectActivity

---
+++ Android/app/src/main/java/com/example/leedayeon/listdetail/DetailSubjectActivity.java
@@ -0,0 +1,141 @@
+package com.example.leedayeon.listdetail;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.leedayeon.listdetail.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DetailSubjectActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBtJoin;
+
+    private TextView tvTitle;
+    private TextView tvDescription;
+    private TextView tvQuiz;
+
+    private EditText answer;
+
+    private String title;
+    private String desc;
+    private String quiz;
+
+    private DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth mAuth;
+    FirebaseUser user;
+    private String games_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_subject);
+
+        tvTitle = (TextView)findViewById(R.id.tvTitle);
+        tvDescription = (TextView)findViewById(R.id.tvDescription);
+        tvQuiz = (TextView)findViewById(R.id.tvQuiz);
+        answer = (EditText)findViewById(R.id.answer);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+
+
+        Intent intent2 = getIntent();
+        games_id = intent2.getStringExtra("games_id");
+
+        myRef.child("games").child(games_id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, String> map = (Map)dataSnapshot.getValue();
+                title = map.get("title");
+                tvTitle.setText(title);
+
+                desc = map.get("description");
+                tvDescription.setText(desc);
+
+                quiz = map.get("subj");
+                tvQuiz.setText(quiz);
+
+                // date = map.get("end_time");
+                // tvMsg.setText(date);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef.child("games").child(games_id).child("participant").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+//                Log.e("Count " ,""+dataSnapshot);
+//                Map<String, String> map = (Map)dataSnapshot.getValue();
+
+                for (DataSnapshot friendSnapshot: dataSnapshot.getChildren()) {
+                    Log.e("uid :", title +"" + friendSnapshot.getKey());
+                    if(user.getUid().equals(friendSnapshot.getKey())){
+                        mBtJoin.setText(getString(R.string.joining));
+                        mBtJoin.setEnabled(false);
+
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        mBtJoin = (Button) findViewById(R.id.btJoin);
+        //mBtJoin.setOnClickListener(this);
+        //여기 오류생기는 건 좀 찾아봐야할것 같아..;; 고대로 복붙했는데 오류나서
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == mBtJoin) {
+
+                myRef.child("games").child(games_id).child("participant").child(user.getUid()).child("answer").setValue(answer.getText().toString());
+                mBtJoin.setText(getString(R.string.joining));
+                mBtJoin.setEnabled(false);
+
+            //Item3.setEnabled(false);
+
+//            Map<String, Object> childUpdates = new HashMap<>();
+//            childUpdates.put("/games/" + games_id, result);
+//
+//            myRef.updateChildren(childUpdates);
+//            myRef.child("games").child(games_id).child("owner").setValue("dongduk");
+
+
+        }
+    }
+}
---
+++ Android/app/src/main/java/com/example/leedayeon/listdetail/DetailSubjectActivity2.java
@@ -0,0 +1,118 @@
+package com.example.leedayeon.listdetail;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+public class DetailSubjectActivity2 extends AppCompatActivity  {
+    private TextView tvTitle;
+    private TextView tvDescription;
+    private TextView tvMsg;
+    private TextView tvQuiz;
+    private Button btEnter;
+
+    private DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
+
+    private String games_id;
+
+    private String title;
+    private String desc;
+    private String quiz;
+
+    private int temp;
+
+    NewQuiz post;
+
+    //final Date date = new Date(post.getEnd_time());
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_subject2);
+
+        tvTitle = (TextView)findViewById(R.id.tvTitle);
+        tvDescription = (TextView)findViewById(R.id.tvDescription);
+        tvMsg = (TextView)findViewById(R.id.tvMsg);
+        tvQuiz = (TextView)findViewById(R.id.tvQuiz);
+        btEnter = (Button)findViewById(R.id.btEnter);
+
+
+        Intent intent2 = getIntent();
+        games_id = intent2.getStringExtra("games_id");
+
+
+        //  SimpleDateFormat dt = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분");
+        // tvMsg.setText(dt.format(date));
+
+        SimpleDateFormat dt = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분");
+        //tvMsg.setText(dt.format(date));
+
+        myRef.child("games").child(games_id).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, String> map = (Map)dataSnapshot.getValue();
+                title = map.get("title");
+                Log.e("hhahahah", title);
+                tvTitle.setText(title);
+
+                desc = map.get("description");
+                tvDescription.setText(desc);
+
+                //dt = map.get("end_time");
+                quiz = map.get("subj");
+                tvQuiz.setText(quiz);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        // tvTitle.setText(title);
+        //tvDescription.setText(intent2.getStringExtra("desc"));
+
+        btEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                final EditText answer = new EditText(DetailSubjectActivity2.this);
+                new AlertDialog.Builder(DetailSubjectActivity2.this)
+                        .setTitle("정답을 입력해 주세요.")
+                        .setView(answer)
+                        .setPositiveButton("입력", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                String value = answer.getText().toString();
+                                myRef.child("games").child(games_id).child("right_answer").setValue(value);
+                                Toast.makeText(getApplicationContext(), value + "선택", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("취소", null)
+                        .show();
+            }
+        });
+
+
+    }
+
+
+}
